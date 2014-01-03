1de7d482234e5fb9c871a50300480d13cc165d7d
Craig LeClair <craigleclair4@gmail.com>
2014-01-03 14:33:02 -0800

    js loading issues potentially fixed.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
 //= require highcharts
 //= require highcharts/highcharts-more
-//= require wiselinks
+//= require turbolinks
 //= require foundation
 //= require_tree .
 
